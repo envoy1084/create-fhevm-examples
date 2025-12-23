36e64d47b160eb506e34157a7418aeb61cde2324
Vedant Chainani <vedantchainani1084@gmail.com>
2025-12-24 03:20:15 +0530

    chore: add protocol imports

--- packages/cli/scripts/postbuild.mjs
+++ packages/cli/scripts/postbuild.mjs
@@ -1,5 +1,5 @@
-import path from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 import folderSize from "fast-folder-size";
 import fs from "fs-extra";
